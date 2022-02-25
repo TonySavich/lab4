e7d4dc5908f69e88a1651376db993239755397df
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-02-25 20:49:58 +0200

    Update Header.hpp

--- Header.hpp
+++ Header.hpp
@@ -1,1 +1,34 @@
+#pragma once
+#include <iostream>
+#include <random>
+#include <chrono>
 
+struct T_List
+{
+	T_List* next;
+	int age;
+};
+void ADD(T_List* head, int age); 
+void PRINT(T_List* head); 
+void DELETE(T_List* head, int k);
+void CLEAR(T_List* head);
+void vstavka(T_List* head, int x, int k);
+class Timer
+{
+private:
+    using clock_t = std::chrono::high_resolution_clock;
+    using second_t = std::chrono::duration<double, std::ratio<1> >;
+    std::chrono::time_point<clock_t> m_beg;
+public:
+    Timer() : m_beg(clock_t::now())
+    {
+    }
+    void reset()
+    {
+        m_beg = clock_t::now();
+    }
+    double elapsed() const
+    {
+        return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
+    }
+};
